2b70c9a58c329d945dce47bbfb1bb09862ddad4f
Yogesh K Gautam <yogi.2210@gmail.com>
2022-12-23 23:14:54 +0530

    Update intersection.cpp

--- Arrays/intersection.cpp
+++ Arrays/intersection.cpp
@@ -1,4 +1,4 @@
-// intersection means common in both of these arrays.
+// intersection means common in both of these arrays. also can not count any number twice
 
 
 Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
